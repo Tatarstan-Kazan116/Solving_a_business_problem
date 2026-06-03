61d1cb323df4584a71cdec30cebcfec2edce9a22
almazsiraziev1@gmail.com <almazsiraziev1@gmail.com>
2026-06-03 15:19:31 +0300

    настроил библиотеку через cmake, пока ошибок нет

--- Solving_a_business_problem.cpp
+++ Solving_a_business_problem.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
 #include <string>
 #include <cctype>
-#include <duckx.hpp>
+#include "duckx.hpp"
 using namespace std;
 int main()
 {
 	setlocale(LC_CTYPE, "Russian");
+	cout << "Успешно";
 	
 }
 
--- build/_deps/duckx-build/cmake_install.cmake
+++ build/_deps/duckx-build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/DuckxExample")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Solving_a_business_problem")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
